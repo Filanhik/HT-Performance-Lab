fa17330e4fc8c406279bdadf35edc42c3f3a1dac
Filanhik <filan.kad@gmail.com>
2025-05-01 07:48:12 +0700

    Add files via upload

---
+++ task1/task1.py
@@ -0,0 +1,20 @@
+n = int(input("Введите количество чисел (n): "))
+m = int(input("Введите длину шага (m): "))
+
+current_num = 1
+
+path = ""
+
+while True:
+    path = path + str(current_num)
+    next_num = current_num + (m - 1)
+
+    while next_num > n:
+        next_num = next_num - n
+
+    if next_num == 1:
+        break
+
+    current_num = next_num
+
+print("Путь: ", path)
